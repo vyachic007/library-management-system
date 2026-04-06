11ad3b7c28756182a9a2c7f7adf21b81e820b13a
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-06 23:51:48 +0300

    Добавил Docker Compose и настроил миграции Liquibase

---
+++ src/main/java/by/slava_borisov/library/config/DatabaseConfig.java
@@ -0,0 +1,29 @@
+package by.slava_borisov.library.config;
+
+import javax.sql.DataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(System.getenv("DB_URL"));
+        dataSource.setUsername(System.getenv("DB_USERNAME"));
+        dataSource.setPassword(System.getenv("DB_PASSWORD"));
+        return dataSource;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db.changelog/changelog-master.yaml");
+        return liquibase;
+    }
+}

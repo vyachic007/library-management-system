94c82802ebba81bba0c10c7c85d10f30f2af6242
Vyacheslav Borisov <vyachic007@gmail.com>
2026-06-17 21:51:07 +0300

    Добавил контроллер экземпляров книг

---
+++ src/main/java/by/slava_borisov/library/controller/rest/BookCopyController.java
@@ -0,0 +1,79 @@
+package by.slava_borisov.library.controller.rest;
+
+import by.slava_borisov.library.dto.request.BookCopyCreateRequestDto;
+import by.slava_borisov.library.dto.request.BookCopyStatusUpdateRequestDto;
+import by.slava_borisov.library.dto.request.BookCopyUpdateRequestDto;
+import by.slava_borisov.library.dto.response.BookCopyResponseDto;
+import by.slava_borisov.library.service.BookCopyService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/book-copies")
+@RequiredArgsConstructor
+public class BookCopyController {
+
+    private final BookCopyService bookCopyService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookCopyResponseDto createBookCopy(
+            @Valid @RequestBody BookCopyCreateRequestDto request
+    ) {
+        return bookCopyService.create(request);
+    }
+
+    @GetMapping
+    public List<BookCopyResponseDto> getAllBookCopies() {
+        return bookCopyService.getAll();
+    }
+
+    @GetMapping("/{copyId}")
+    public BookCopyResponseDto getBookCopyById(
+            @PathVariable Long copyId
+    ) {
+        return bookCopyService.getById(copyId);
+    }
+
+    @GetMapping("/book/{bookId}")
+    public List<BookCopyResponseDto> getBookCopiesByBookId(
+            @PathVariable Long bookId
+    ) {
+        return bookCopyService.getByBookId(bookId);
+    }
+
+    @GetMapping("/book/{bookId}/available")
+    public List<BookCopyResponseDto> getAvailableBookCopiesByBookId(
+            @PathVariable Long bookId
+    ) {
+        return bookCopyService.getAvailableByBookId(bookId);
+    }
+
+    @PutMapping("/{copyId}")
+    public BookCopyResponseDto updateBookCopy(
+            @PathVariable Long copyId,
+            @Valid @RequestBody BookCopyUpdateRequestDto request
+    ) {
+        return bookCopyService.update(copyId, request);
+    }
+
+    @PatchMapping("/{copyId}/status")
+    public BookCopyResponseDto changeBookCopyStatus(
+            @PathVariable Long copyId,
+            @Valid @RequestBody BookCopyStatusUpdateRequestDto request
+    ) {
+        return bookCopyService.changeStatus(copyId, request);
+    }
+
+    @DeleteMapping("/{copyId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeBookCopyById(
+            @PathVariable Long copyId
+    ) {
+        bookCopyService.delete(copyId);
+    }
+}

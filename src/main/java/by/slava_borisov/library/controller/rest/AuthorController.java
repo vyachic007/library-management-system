18676850d5c62b32e37e154ac46596b890020b6a
Vyacheslav Borisov <vyachic007@gmail.com>
2026-06-17 23:08:29 +0300

    Добавил контроллер авторов

---
+++ src/main/java/by/slava_borisov/library/controller/rest/AuthorController.java
@@ -0,0 +1,63 @@
+package by.slava_borisov.library.controller.rest;
+
+import by.slava_borisov.library.dto.request.AuthorCreateRequestDto;
+import by.slava_borisov.library.dto.request.AuthorUpdateRequestDto;
+import by.slava_borisov.library.dto.response.AuthorResponseDto;
+import by.slava_borisov.library.service.AuthorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/authors")
+@RequiredArgsConstructor
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AuthorResponseDto createAuthor(
+            @Valid @RequestBody AuthorCreateRequestDto request
+    ) {
+        return authorService.create(request);
+    }
+
+    @GetMapping
+    public List<AuthorResponseDto> getAllAuthors() {
+        return authorService.getAll();
+    }
+
+    @GetMapping("/search")
+    public List<AuthorResponseDto> getAuthorsByLastName(
+            @RequestParam String lastName
+    ) {
+        return authorService.getByLastName(lastName);
+    }
+
+    @GetMapping("/{authorId}")
+    public AuthorResponseDto getAuthorById(
+            @PathVariable Long authorId
+    ) {
+        return authorService.getById(authorId);
+    }
+
+    @PutMapping("/{authorId}")
+    public AuthorResponseDto updateAuthor(
+            @PathVariable Long authorId,
+            @Valid @RequestBody AuthorUpdateRequestDto request
+    ) {
+        return authorService.update(authorId, request);
+    }
+
+    @DeleteMapping("/{authorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeAuthorById(
+            @PathVariable Long authorId
+    ) {
+        authorService.delete(authorId);
+    }
+}

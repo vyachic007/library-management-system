6e429a1b7cc25d7acf6f455a3f1bb5509d28e20f
Vyacheslav Borisov <vyachic007@gmail.com>
2026-06-19 19:32:08 +0300

    Добавил контроллер категорий

---
+++ src/main/java/by/slava_borisov/library/controller/rest/CategoryController.java
@@ -0,0 +1,68 @@
+package by.slava_borisov.library.controller.rest;
+
+import by.slava_borisov.library.dto.request.CategoryCreateRequestDto;
+import by.slava_borisov.library.dto.request.CategoryUpdateRequestDto;
+import by.slava_borisov.library.dto.response.CategoryResponseDto;
+import by.slava_borisov.library.service.CategoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryResponseDto createCategory(
+            @Valid @RequestBody CategoryCreateRequestDto request
+    ) {
+        return categoryService.create(request);
+    }
+
+    @PutMapping("/{categoryId}")
+    public CategoryResponseDto updateCategory(
+            @PathVariable Long categoryId,
+            @Valid @RequestBody CategoryUpdateRequestDto request
+    ) {
+        return categoryService.update(categoryId, request);
+    }
+
+    @GetMapping
+    public List<CategoryResponseDto> getAllCategories() {
+        return categoryService.getAll();
+    }
+
+    @GetMapping("/root")
+    public List<CategoryResponseDto> getRootCategories() {
+        return categoryService.getRootCategories();
+    }
+
+    @GetMapping("/{parentId}/subcategories")
+    public List<CategoryResponseDto> getSubcategoriesByParentId(
+            @PathVariable Long parentId
+    ) {
+        return categoryService.getByParentId(parentId);
+    }
+
+    @GetMapping("/{categoryId}")
+    public CategoryResponseDto getCategoryById(
+            @PathVariable Long categoryId
+    ) {
+        return categoryService.getById(categoryId);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeCategoryById(
+            @PathVariable Long categoryId
+    ) {
+        categoryService.delete(categoryId);
+    }
+}

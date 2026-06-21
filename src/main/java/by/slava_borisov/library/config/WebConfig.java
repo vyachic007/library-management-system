cae573e1bd8e32062d509bc58d88173f70eb8924
Vyacheslav Borisov <vyachic007@gmail.com>
2026-06-21 19:17:55 +0300

    Настроил единый формат сериализации дат

--- src/main/java/by/slava_borisov/library/config/WebConfig.java
+++ src/main/java/by/slava_borisov/library/config/WebConfig.java
@@ -1,14 +1,34 @@
 package by.slava_borisov.library.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
+@RequiredArgsConstructor
 @ComponentScan({
         "by.slava_borisov.library.controller",
         "by.slava_borisov.library.exception"
 })
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void extendMessageConverters(
+            List<HttpMessageConverter<?>> converters
+    ) {
+        converters.stream()
+                .filter(MappingJackson2HttpMessageConverter.class::isInstance)
+                .map(MappingJackson2HttpMessageConverter.class::cast)
+                .forEach(converter -> converter.setObjectMapper(objectMapper));
+    }
 }
